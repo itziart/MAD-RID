b9a379f0aac54189533fb44ab2aa7456907ed4ac
banacchini <116755374+banacchini@users.noreply.github.com>
2024-12-01 23:35:37 +0100

    Build Bug Fix
    Fixed a bug where the intro animation wouldn't play due to the interaction with the SplashScreen.

--- Assets/Scripts/ScreenManaging/IntroSceneManager.cs
+++ Assets/Scripts/ScreenManaging/IntroSceneManager.cs
@@ -3,10 +3,23 @@ using UnityEngine.SceneManagement; // Required for scene management
 
 public class IntroSceneManager : MonoBehaviour
 {
+    private Animator animator;
     // Function to change scene after animation finishes
     public void ChangeScene()
     {
         // Load the Start Screen Scene
         SceneManager.LoadScene("StartScreen");
+    }
+
+    void Start()
+    {
+        {
+            animator = GetComponent<Animator>();    
+            if (animator != null)
+            {
+                animator.SetTrigger("TRStartAnimation");
+            }
+
+        }
     }
 }
